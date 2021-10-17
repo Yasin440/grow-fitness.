49e9e18280e30eab2538b433eef6a799cd2b943d
Yasin440 <eiasin440@gmail.com>
2021-10-18 01:00:16 +0600

    header

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="navbar navbar-expand-lg navbar-light header">
+            <div className="container">
+                <img className='logo' src='https://i.ibb.co/rb8w8Lj/Fitness-logo.png' alt='logo' />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="pages navbar-nav me-auto mb-2 mb-lg-0">
+                            <NavLink to='/home'>Home</NavLink>
+                            <NavLink to='/Team'>Team</NavLink>
+                            <NavLink to='/about'>About Us</NavLink>
+                            <NavLink to='/contact'>Contact Us</NavLink>
+                    </div>
+                    <form className="d-flex">
+                        <button className="signUp">Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBd5Oi5qJXzPIeHkI4_P8tNLP6yEIBVgSY",
+    authDomain: "grow-fitness-b6c91.firebaseapp.com",
+    projectId: "grow-fitness-b6c91",
+    storageBucket: "grow-fitness-b6c91.appspot.com",
+    messagingSenderId: "675221656071",
+    appId: "1:675221656071:web:c2d9f6a85f0920cd8810a2"
+  };
+  export default firebaseConfig;
