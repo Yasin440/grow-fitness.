e7be15f7e7f814098d909c38cc36d9b04635d85a
Yasin440 <eiasin440@gmail.com>
2021-10-18 20:08:01 +0600

    firebasr

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,14 +1,7 @@
 import initializeAuthentication from "../Firebase/firebase.init";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 initializeAuthentication();
 
 const useFirebase = () =>{
-    const auth = getAuth()
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-      })
 }
 export default useFirebase;
