5ce5d886260a84824d1327abc7584b52c9a4dfbc
Yasin440 <eiasin440@gmail.com>
2021-10-19 22:48:53 +0600

    done

--- src/Component/Pages/Details/Details.js
+++ src/Component/Pages/Details/Details.js
@@ -1,18 +1,17 @@
 import { useParams } from 'react-router';
-import useAuth from '../../../Hooks/useAuth';
+// import useAuth from '../../../Hooks/useAuth';
 import './Details.css';
 
 const Details = () => {
     const { details_id } = useParams();
-    const {services} = useAuth();
-    const getDetails =  services.filter(service =>service._id ===details_id );
-    console.log(getDetails);
+    // const { services } = useAuth();
+    // const getDetails = services.filter(service => service._id === details_id);
+    // const detail = getDetails[0];
+    // console.log(detail);
+    // const { _id, name } = detail;
     return (
         <div>
             <div className="container text-center mt-5"><h2>details of -id: {details_id}</h2></div>
-            {
-               
-            }
         </div>
     );
 };
